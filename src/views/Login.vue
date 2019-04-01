43f2ac1cbe53ba9e585284b1fc385d9056c53870
unknown <Aluno>
2019-04-01 16:42:33 -0300

    Teste

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         <v-text-field label="Teste"></v-text-field>
-        <v-btn color="red">Teste</v-btn>
+        <v-btn color="red">Teste 2</v-btn>
     </div>
 </template>
